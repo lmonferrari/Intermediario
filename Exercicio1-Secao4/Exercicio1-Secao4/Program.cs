11f05dba118804c17a69b9a797f81e21522c6a5a
lmonferrari <lmonferrari@outlook.com>
2017-05-18 16:09:49 -0300

    More exercices
    Secation 4 exercice

---
+++ Exercicio1-Secao4/Exercicio1-Secao4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercicio1_Secao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            stack.Push(5);
+            Console.WriteLine(stack.Pop());
+
+
+            stack.Push(10);
+            stack.Push(11);
+            stack.Push(51);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            stack.Clear();
+
+            //pop unexisting itens
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            //stack.Push(null);
+        }
+    }
+}
---
+++ Exercicio1-Secao4/Exercicio1-Secao4/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio1_Secao4
+{
+    class Stack
+    {
+        private List<object> _stack;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("push - null");
+
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var count = _stack.Count - 1;
+           
+            if (count < 0)
+                return "nothing to pop.";
+
+            var itemStack = _stack[count];
+            _stack.RemoveAt(count);
+            return itemStack;
+        }
+
+        public void Clear()
+        {
+            _stack.RemoveRange(0, _stack.Count);
+        }
+    }
+}
